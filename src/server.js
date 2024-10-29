66e1c2d246d6d0718fc34325d742d2f622f8fa08
RomanFS76 <korvy85@gmail.com>
2024-10-29 22:18:24 +0200

    modify server.js

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
+
 import { env } from './utils/env.js';
 
-import { getContacts,getContactsById } from './services/contacts.js';
 import routerContacts from './routers/contacts.js';
-
-
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { logger } from './middlewares/logger.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -15,30 +15,13 @@ export const setupServer = () => {
 
   app.use(cors());
 
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
+  app.use(logger);
 
-  app.get('/contacts', routerContacts);
+  app.use('/contacts', routerContacts);
 
+  app.use('*', notFoundHandler);
 
-
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Route not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
