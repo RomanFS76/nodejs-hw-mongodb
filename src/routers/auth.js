867fe2629d80b18ea79749843574bc0b61dd5a26
RomanFS76 <korvy85@gmail.com>
2024-11-21 14:49:20 +0300

    add auth router '/request-reset-email'

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,17 +1,36 @@
 import { Router } from 'express';
-import { loginController, registerController,refreshSessionController,logoutController } from '../controllers/auth.js';
+import {
+  loginController,
+  registerController,
+  refreshSessionController,
+  logoutController,
+  requestResetEmailController,
+} from '../controllers/auth.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import validateBody from '../middlewares/validateBody.js';
-import { loginUserSchema, registerUserSchema } from '../validation/auth.js';
+import { loginUserSchema, registerUserSchema, requestResetEmailSchema } from '../validation/auth.js';
 
 const authRouter = Router();
 
-authRouter.post("/register", validateBody(registerUserSchema), ctrlWrapper(registerController))
-authRouter.post("/login", validateBody(loginUserSchema), ctrlWrapper(loginController))
+authRouter.post(
+  '/register',
+  validateBody(registerUserSchema),
+  ctrlWrapper(registerController),
+);
+authRouter.post(
+  '/login',
+  validateBody(loginUserSchema),
+  ctrlWrapper(loginController),
+);
 
-authRouter.post("/refresh", ctrlWrapper(refreshSessionController))
+authRouter.post('/refresh', ctrlWrapper(refreshSessionController));
 
-authRouter.post("/logout", ctrlWrapper(logoutController))
+authRouter.post('/logout', ctrlWrapper(logoutController));
 
+authRouter.post(
+    '/request-reset-email',
+    validateBody(requestResetEmailSchema),
+    ctrlWrapper(requestResetEmailController),
+  );
 
 export default authRouter;
